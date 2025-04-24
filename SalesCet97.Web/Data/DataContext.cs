83db1129e5bc61958e36afee00f1ae7a7ff75259
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-04-25 00:38:18 +0100

    Create database and countries table.

---
+++ SalesCet97.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCet97.Web.Data.Entities;
+
+namespace SalesCet97.Web.Data
+{
+    public class DataContext : DbContext
+    {
+
+        //coloco dentro das opções o meu datacontex que eu vou fazer e passo para a base, estou a injetar o meu datacontex dentro da base e passar para lá.
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
